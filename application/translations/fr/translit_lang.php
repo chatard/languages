00bfbcd68cba3e71ebc66e582f8c44349fe217b0
chatard <jacques.chatard@gmail.com>
2013-01-17 01:12:25 -0800

    Create translit_lang.php
    This file allows the replacement of special characters in the url.

---
+++ application/translations/fr/translit_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Character list for replacement in SEO URL's
+ * @var array SEO replacement list
+**/
+
+$aSeoReplaceChars = array(
+    'é' => 'e',
+    'è' => 'e',
+    'ê' => 'e',
+    'ç' => 'c',
+    'à' => 'a',
+    'ù' => 'u',
+);
